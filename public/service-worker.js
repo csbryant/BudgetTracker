4ec739e358637bea8a86d1a97ab823c33a3f0308
Chris Bryant <chris_bryant@mac.com>
2020-11-04 11:28:30 -0800

    Updated Service worker to include API and added screenshots

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,22 @@
-const filesToCache = [
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const staticFilesToPreCache = [
   "/",
-  "index.jtml",
+  "index.html",
   "index.js",
   "styles.css",
   "db.js",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "/manifest.webmanifest",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log("Your files were pre-cached successfully!");
-      return cache.addAll(filesToCache);
+      return cache.addAll(staticFilesToPreCache);
     })
   );
 
@@ -36,34 +36,37 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
+
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function (evt) {
-  const { url } = evt.request;
-  if (url.includes("/all") || url.includes("/find")) {
+self.addEventListener('fetch', function (evt) {
+  if (evt.request.url.includes('/api/')) {
+    console.log('[Service Worker] Fetch (data)', evt.request.url);
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
             if (response.status === 200) {
-              cache.put(evt.request, response.clone());
+              cache.put(evt.request.url, response.clone());
             }
-
             return response;
           })
           .catch(err => {
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
-    );
-  } else {
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
+      }).catch(err => {
+        console.log(err)
       })
     );
+
+    return;
   }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
